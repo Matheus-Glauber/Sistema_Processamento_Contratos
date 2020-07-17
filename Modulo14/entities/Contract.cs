d15c7cdcea2ce40a7401c4dc91a7b21c9a729884
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@gmail.com>
2020-07-17 18:19:06 -0300

    Entidades Contract e Installment criados.

---
+++ Modulo14/entities/Contract.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modulo14.entities
+{
+    class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public List<Installment> Installments { get; set; }
+
+        public Contract()
+        {
+        }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+
+        public Contract(int number, DateTime date, double totalValue, List<Installment> installments)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installments = installments;
+        }
+    }
+}
---
+++ Modulo14/entities/Installment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modulo14.entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment()
+        {
+        }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
